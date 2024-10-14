d93add9b64b1a74c5457186f4fa2638310b71a51
gideonkkk <gideon.kariuki@student.moringaschool.com>
2024-10-14 11:10:16 +0300

    completed

--- server/app.py
+++ server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, make_response, jsonify
+from flask import Flask, request, make_response, jsonify, abort
 from flask_cors import CORS
 from flask_migrate import Migrate
 
@@ -14,13 +14,48 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
-def messages():
-    return ''
+@app.route('/messages', methods=['GET'])
+def get_messages():
+    messages = Message.query.order_by(Message.created_at.asc()).all()
+    return jsonify([message.to_dict() for message in messages])
 
-@app.route('/messages/<int:id>')
-def messages_by_id(id):
-    return ''
+# Route to create a new message
+@app.route('/messages', methods=['POST'])
+def create_message():
+    data = request.get_json()
+    body = data.get('body')
+    username = data.get('username')
+    
+    if not body or not username:
+        return abort(400, description="Both 'body' and 'username' are required")
+    
+    message = Message(body=body, username=username)
+    db.session.add(message)
+    db.session.commit()
+    
+    return jsonify(message.to_dict()), 201
+
+# Route to update a message
+@app.route('/messages/<int:id>', methods=['PATCH'])
+def update_message(id):
+    message = db.session.get(Message, id) 
+    data = request.get_json()
+    
+    body = data.get('body')
+    if body:
+        message.body = body
+        db.session.commit()
+    
+    return jsonify(message.to_dict())
+
+# Route to delete a message
+@app.route('/messages/<int:id>', methods=['DELETE'])
+def delete_message(id):
+    message = db.session.get(Message, id) 
+    db.session.delete(message)
+    db.session.commit()
+    
+    return jsonify({"message": "Message deleted successfully"}), 200
 
 if __name__ == '__main__':
     app.run(port=5555)
